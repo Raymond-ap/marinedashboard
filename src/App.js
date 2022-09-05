792fb06cf61a3be849b6a9020555a21a8fb7aa25
Apungu Raymond <apunguray@gmail.com>
2022-09-06 04:15:59 +0800

    local update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,59 @@
-import { Switch, Route, Redirect } from 'react-router-dom';
-import Sidebar from 'components/Sidebar';
-import Dashboard from 'pages/Dashboard';
-import Settings from 'pages/Settings';
-import Tables from 'pages/Tables';
-import Maps from 'pages/Maps';
-import Footer from 'components/Footer';
+import { Route, Redirect, Switch } from "react-router-dom";
+import Sidebar from "./layouts/Sidebar";
+import Dashboard from "pages/Dashboard";
+import Settings from "pages/Settings";
+import Tables from "pages/Tables";
+import Maps from "pages/Maps";
+import Footer from "components/Footer";
+import Login from "pages/auth/Login";
+import Signup from "pages/auth/Signup";
+import ResetPassword from "pages/auth/ResetPassword";
 
 // Tailwind CSS Style Sheet
-import 'assets/styles/tailwind.css';
+import "assets/styles/tailwind.css";
+
+// const AuthRoute = () => {
+//   return (
+//     <>
+//       <Route exact path="/" component={Login} />
+//       <Route path="/signup" component={Signup} />
+//       <Route path="/reset-password" component={ResetPassword} />
+//     </>
+//   );
+// };
+
+// const DashboardRoute = () => {
+//   return (
+//     <>
+//       <Sidebar />
+//       <div className="relative md:ml-64 bg-blueGray-100">
+//         <Route path="/" component={Dashboard} />
+//         <Route path="/settings" component={Settings} />
+//         <Route path="/tables" component={Tables} />
+//         <Route path="/maps" component={Maps} />
+//         <Footer />
+//       </div>
+//     </>
+//   );
+// };
 
 function App() {
-    return (
-        <>
-            <Sidebar />
-            <div className="md:ml-64 ">
-                <Switch>
-                    <Route exact path="/" component={Dashboard} />
-                    <Route exact path="/settings" component={Settings} />
-                    <Route exact path="/tables" component={Tables} />
-                    <Route exact path="/maps" component={Maps} />
-                    <Redirect from="*" to="/" />
-                </Switch>
-            </div>
-        </>
-    );
+  return (
+    <>
+      {/* <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/reset-password" component={ResetPassword} />
+      </Switch> */}
+      <Sidebar />
+      <div className="relative md:ml-64 bg-blueGray-100">
+        <Route path="/" component={Dashboard} />
+        <Route path="/settings" component={Settings} />
+        <Route path="/tables" component={Tables} />
+        <Route path="/maps" component={Maps} />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/layouts/AdminNavbar.js
@@ -0,0 +1,63 @@
+import { useLocation } from 'react-router-dom';
+import Button from '@material-tailwind/react/Button';
+import Icon from '@material-tailwind/react/Icon';
+import NavbarInput from '@material-tailwind/react/NavbarInput';
+import Image from '@material-tailwind/react/Image';
+import Dropdown from '@material-tailwind/react/Dropdown';
+import DropdownItem from '@material-tailwind/react/DropdownItem';
+import ProfilePicture from 'assets/img/team-1-800x800.jpg';
+
+export default function AdminNavbar({ showSidebar, setShowSidebar }) {
+    const location = useLocation().pathname;
+
+    return (
+        <nav className="bg-white shadow-lg md:ml-64 py-6 px-3">
+            <div className="container max-w-full mx-auto flex items-center justify-between md:pr-8 md:pl-10">
+                <div className="md:hidden">
+                    <Button
+                        color="transparent"
+                        buttonType="link"
+                        size="lg"
+                        iconOnly
+                        rounded
+                        ripple="light"
+                        onClick={() => setShowSidebar('left-0')}
+                    >
+                        <Icon name="menu" size="2xl" color="black" />
+                    </Button>
+                    <div
+                        className={`absolute top-2 md:hidden ${
+                            showSidebar === 'left-0' ? 'left-64' : '-left-64'
+                        } z-50 transition-all duration-300`}
+                    >
+                        <Button
+                            color="transparent"
+                            buttonType="link"
+                            size="lg"
+                            iconOnly
+                            rounded
+                            ripple="light"
+                            onClick={() => setShowSidebar('-left-64')}
+                        >
+                            <Icon name="close" size="2xl" color="black" />
+                        </Button>
+                    </div>
+                </div>
+
+                <div className="flex justify-between items-center w-full">
+                    <h4 className="uppercase text-black text-sm tracking-wider mt-1">
+                        {location === '/'
+                            ? 'DASHBOARD'
+                            : location.toUpperCase().replace('/', '')}
+                    </h4>
+
+                    <div className="flex">
+                        <button className='bg-green-500 uppercase text-sm py-1 px-4 text-white font-semibold rounded-md' >
+                            premiun
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
